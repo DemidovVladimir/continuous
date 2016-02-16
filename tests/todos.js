2871679d68e03bc3d15b8e0e585c4785341b618c
vladimir050486 <uncojet@gmail.com>
2016-02-16 23:56:15 +0100

    hi

---
+++ tests/todos.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+suite('Todos', function() {
+    test('Metor server side', function(done, server) {
+        server.eval(function() {
+            Todos.insert({name: 'try out Meteor!'});
+            var docs = Todos.find().fetch();
+            emit('docs', docs);
+        });
+
+        server.once('docs', function(docs) {
+            assert.equal(docs.length, 1);
+            assert.equal(docs[0].name, 'try out Meteor!');
+            done();
+        });
+    });
+});
